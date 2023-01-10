7c05f8b4c92d9541b335d626d3b09ad32795cb71
Foodpunch <foodpunch@gmail.com>
2023-01-10 11:47:08 +0800

    Alpha Engine base
    -Proj settings etc.

---
+++ Main.cpp
@@ -0,0 +1,61 @@
+// ---------------------------------------------------------------------------
+// includes
+
+#include "AEEngine.h"
+
+
+
+// ---------------------------------------------------------------------------
+// main
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+
+	int gGameRunning = 1;
+
+	// Initialization of your own variables go here
+
+	// Using custom window procedure
+	AESysInit(hInstance, nCmdShow, 800, 600, 1, 60, true, NULL);
+
+	// Changing the window title
+	AESysSetWindowTitle("My New Demo!");
+
+	// reset the system modules
+	AESysReset();
+
+
+
+	// Game Loop
+	while (gGameRunning)
+	{
+		// Informing the system about the loop's start
+		AESysFrameStart();
+
+		// Handling Input
+		AEInputUpdate();
+
+		// Your own update logic goes here
+
+
+		// Your own rendering logic goes here
+
+
+		// Informing the system about the loop's end
+		AESysFrameEnd();
+
+		// check if forcing the application to quit
+		if (AEInputCheckTriggered(AEVK_ESCAPE) || 0 == AESysDoesWindowExist())
+			gGameRunning = 0;
+	}
+
+
+	// free the system
+	AESysExit();
+}
