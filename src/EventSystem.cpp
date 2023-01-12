13b7b859f54a24cd5191c587e7720196a966ca9d
JarrettAng <jarrett_ang@hotmail.com>
2023-01-12 20:54:47 +0800

    Fixed EventSystem
    Project can compile now + added support for functions with no parameters

--- src/EventSystem.cpp
+++ src/EventSystem.cpp
@@ -1,103 +1,158 @@
-// 2 usage examples of events
-/*
-    === Example 1: Printing score to cout whenever it updates ===
+/*!************************************************************************
+\file EventSystem.cpp
+\author Ang Jiawei Jarrett
+\par DP email: a.jiaweijarrett@digipen.edu
+\par Course: CSD1171B
+\par Programming Lab #1
+\date 12/01/2023
+\brief
+This source file implements a generic event system that allows functions with
+no parameters/ 1 parameter of any type. Below are 2 usage examples of these events.
 
-    Step 1: Define the event wherever the score is handled
-    EventSystem::Event<int> onScoreUpdate;
+======== Example 1: Printing score to cout whenever it updates ========
 
-    Step 2: In the printing file, subscribe to event
-    void printScore() {
-        onScoreUpdate.Subscribe(scorePrintFunc);
+Step 1: Define the event variable wherever the score is handled
+EventSystem::Event<int> onScoreUpdate;
 
-        -or-
+Step 2: In the printing file, subscribe to event (You can expose it in a header file or something)
+void start() {
+    onScoreUpdate.Subscribe(scorePrintFunc);
+    -or-
+    onScoreUpdate.Subscribe(scorePrintFunc, 10); // Optional: second parameter for priority
+}
 
-        onScoreUpdate.Subscribe(scorePrintFunc, 10); // Optional: second parameter for priority
-    }
+Step 3: Whoever is handling the score will invoke/call the event
+void addScore() {
+    ...
+    onScoreUpdate.Invoke(score);
+}
 
-    Step 3: Whoever is handling the score will invoke/call the event
-    void addScore() {
-        ...
-        onScoreUpdate.Invoke(score);
-    }
+Step 4: All functions subscribed to the event will be called in order of priority
+[console] new score: 100
 
-    Step 4: All functions subscribed to the event will be called in order of priority
-    [console] new score: 100
+======== Example 2: Have fireworks play once on the first building placed ========
 
+Step 1: Define the event wherever the building placing is handled
+EventSystem::Event<void> onBuildingPlaced;
 
-    === Example 2: Have fireworks play once on the first building placed ===
+Step 2: In the file that plays fireworks, subscribe to event (You can expose it in a header file or something)
+void start() {
+    onBuildingPlaced.Subscribe(launchFireworks);
+    -or-
+    onBuildingPlaced.Subscribe(launchFireworks, -50); // Optional: second parameter for priority
+}
 
-    Step 1: Define the event wherever the building placing is handled
-    EventSystem::Event<void> onBuildingPlaced;
+Step 3: Whoever is handling the building placement will invoke/call the event
+void placeBuilding() {
+    ...
+    onBuildingPlaced.Invoke();
+}
 
-    Step 2: In the file, subscribe to event
-    void printScore() {
-        onBuildingPlaced.Subscribe(launchFireworks);
+Step 4: In the function subscribed to the event, unsubscribe itself
+void launchFireworks() {
+    ...
+    onBuildingPlaced.Unsubscribe(launchFireworks);
+}
+**************************************************************************/
 
-        -or-
-
-        onBuildingPlaced.Subscribe(launchFireworks, -50); // Optional: second parameter for priority
-    }
-
-    Step 3: Whoever is handling the building placement will invoke/call the event
-    void placeBuilding() {
-        ...
-        onBuildingPlaced.Invoke();
-    }
-
-    Step 4: In the function subscribed to the event, unsubscribe itself
-    void launchFireworks() {
-        ...
-        onBuildingPlaced.Unsubscribe(launchFireworks);
-    }
-*/
-
-#include <iostream>
+#include <list>
 
 namespace EventSystem {
-    template<class T> struct EventData {
+    template<class T> 
+    struct EventData {
         void (*fun_ptr)(T);
         int priority;
     };
 
-    template<class T> class Event {
-    private: std::list<EventData<T>> subscribers;
+    template<>
+    struct EventData<void> {
+        void (*fun_ptr)();
+        int priority;
+    };
+    
+    template<class T> 
+    class Event {
+        private: std::list<EventData<T>> subscribers;
 
-    public: void Invoke(T var) {
-        for (auto i = subscribers.begin(); i != subscribers.end(); ++i) {
-            (i->fun_ptr)(var);
-        }
-    }
-
-    public: void Subscribe(void (*fun_ptr)(T), int priority) {
-        EventData<T> new_sub = { fun_ptr, priority };
-
-        if (subscribers.size() >= 1) {
+        public: void Invoke(T var) {
             for (auto i = subscribers.begin(); i != subscribers.end(); ++i) {
-                if (i->priority <= priority) {
-                    subscribers.insert(i, new_sub);
-                    return;
+                (i->fun_ptr)(var);
+            }
+        }
+
+        public: void Subscribe(void (*fun_ptr)(T), int priority) {
+            EventData<T> new_sub = { fun_ptr, priority };
+
+            if (subscribers.size() >= 1) {
+                for (auto i = subscribers.begin(); i != subscribers.end(); ++i) {
+                    if (i->priority <= priority) {
+                        subscribers.insert(i, new_sub);
+                        return;
+                    }
+                }
+            }
+
+            subscribers.push_back(new_sub);
+        }
+
+        public: void Subscribe(void (*fun_ptr)(T)) {
+            Subscribe(fun_ptr, 0);
+        }
+
+        public: void Unsubscribe(void (*fun_ptr)(T)) {
+            for (auto i = subscribers.begin(); i != subscribers.end(); ++i) {
+                if (i->fun_ptr == fun_ptr) {
+                    subscribers.erase(i);
+                    break;
                 }
             }
         }
 
-        subscribers.push_back(new_sub);
-    }
+        public: void UnsubscribeAll() {
+            subscribers.clear();
+        }
+    };
 
-    public: void Subscribe(void (*fun_ptr)(T)) {
-        Subscribe(fun_ptr, 0);
-    }
+    template<>
+    class Event<void> {
+        private: std::list<EventData<void>> subscribers;
 
-    public: void Unsubscribe(void (*fun_ptr)(T)) {
-        for (auto i = subscribers.begin(); i != subscribers.end(); ++i) {
-            if (i->fun_ptr == fun_ptr) {
-                subscribers.erase(i);
-                break;
+        public: void Invoke() {
+            for (auto i = subscribers.begin(); i != subscribers.end(); ++i) {
+                (i->fun_ptr)();
             }
         }
-    }
 
-    public: void UnsubscribeAll() {
-        subscribers.clear();
-    }
+        public: void Subscribe(void (*fun_ptr)(), int priority) {
+            EventData<void> new_sub = { fun_ptr, priority };
+
+            if (subscribers.size() >= 1) {
+                for (auto i = subscribers.begin(); i != subscribers.end(); ++i) {
+                    if (i->priority <= priority) {
+                        subscribers.insert(i, new_sub);
+                        return;
+                    }
+                }
+            }
+
+            subscribers.push_back(new_sub);
+        }
+
+        public: void Subscribe(void (*fun_ptr)()) {
+            Subscribe(fun_ptr, 0);
+        }
+
+        public: void Unsubscribe(void (*fun_ptr)()) {
+            for (auto i = subscribers.begin(); i != subscribers.end(); ++i) {
+                if (i->fun_ptr == fun_ptr) {
+                    subscribers.erase(i);
+                    break;
+                }
+            }
+        }
+
+        public: void UnsubscribeAll() {
+            subscribers.clear();
+        }
     };
 }
