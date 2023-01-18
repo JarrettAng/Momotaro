bfa435c7c076c8b556f00ca80a8be82a482c9768
JunRong19 <tanjunrong321@gmail.com>
2023-01-18 19:39:18 +0800

    Moved math related stuff to its own file

---
+++ inc/MomoMaths.h
@@ -0,0 +1,35 @@
+/*!************************************************************************
+\file MomoMaths.hpp
+\author Tan Jun Rong
+\par DP email: t.junrong@digipen.edu
+\par Course: CSD1171B
+\date 18/01/2023
+\brief
+This header file declares
+
+**************************************************************************/
+
+#pragma once
+template<class T>
+struct Vec4
+{
+	T w;
+	T x;
+	T y;
+	T z;
+};
+
+/*!***********************************************************************
+\brief
+	Compare the equality of both Vector 4
+\return
+	Return true if both vector are equal. Else return false.
+*************************************************************************/
+template<class T>
+bool operator==(const Vec4<T>& lhs, const Vec4<T>& rhs)
+{
+	if (lhs.w == rhs.w && lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z) {
+		return true;
+	}
+	return false;
+}
