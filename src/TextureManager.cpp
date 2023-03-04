d6a899357fabbb20910eb3f507f3f2411701e485
JunRong19 <tanjunrong321@gmail.com>
2023-03-04 19:37:37 +0800

    Added Tamago shop + ramen -> lvl 2

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -137,7 +137,7 @@ namespace TextureManager {
 		textures.push_back(TextureSheet{ RESIDENTIAL_1X2_L3, 1, 1, -1 });
 
 		textures.push_back(TextureSheet{ COMMERCIAL_1X1_L1, 4, 6, .15 });
-		textures.push_back(TextureSheet{ COMMERCIAL_1X1_L2, 1, 1, -1 });
+		textures.push_back(TextureSheet{ COMMERCIAL_1X1_L2, 4, 6, .15 });
 		textures.push_back(TextureSheet{ COMMERCIAL_1X1_L3, 1, 1, -1 });
 
 		textures.push_back(TextureSheet{ INDUSTRIAL_1X1_L1, 3, 9, .2 });
