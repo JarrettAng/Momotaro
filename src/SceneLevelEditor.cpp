475a0e922652083b0ea7711ffc1d3aa017386e68
JarrettAng <jarrett_ang@hotmail.com>
2023-03-26 21:09:25 +0800

    Level Editor WIP

--- src/SceneLevelEditor.cpp
+++ src/SceneLevelEditor.cpp
@@ -27,13 +27,35 @@ The functions include:
 #include <PauseManager.h>
 #include <TextureManager.h>
 ///////////////////////////////////////////////////////////////////////////
+// Forward Declarations
+void LoadMap1();
+void LoadMap2();
+void LoadMap3();
+void LoadUserMap1();
+void LoadUserMap2();
+void LoadUserMap3();
+
+///////////////////////////////////////////////////////////////////////////
+// Variables
+std::string selectedMapFilePath;
+
 
 void SceneLevelEditor::Load() {
-	GridManager::Initialize("Assets/JSON_Data/Maps/tutorial.momomaps");
+	BuildingManager::Initialize();
+	LoadMap1();
 }
 
 void SceneLevelEditor::Initialize() {
 	PauseManager::Initialize();
+
+	// Bind keys 1-6 to the different maps
+	InputManager::SubscribeToKey(AEVK_1, InputManager::TRIGGERED, LoadMap1);
+	InputManager::SubscribeToKey(AEVK_2, InputManager::TRIGGERED, LoadMap2);
+	InputManager::SubscribeToKey(AEVK_3, InputManager::TRIGGERED, LoadMap3);
+	InputManager::SubscribeToKey(AEVK_4, InputManager::TRIGGERED, LoadUserMap1);
+	InputManager::SubscribeToKey(AEVK_5, InputManager::TRIGGERED, LoadUserMap2);
+	InputManager::SubscribeToKey(AEVK_6, InputManager::TRIGGERED, LoadUserMap3);
+
 }
 
 void SceneLevelEditor::Update() {
@@ -52,8 +74,53 @@ void SceneLevelEditor::Draw() {
 
 void SceneLevelEditor::Free() {
 	GridManager::Free();
+
+	InputManager::UnsubscribeKey(AEVK_1, InputManager::TRIGGERED, LoadMap1);
+	InputManager::UnsubscribeKey(AEVK_2, InputManager::TRIGGERED, LoadMap2);
+	InputManager::UnsubscribeKey(AEVK_3, InputManager::TRIGGERED, LoadMap3);
+	InputManager::UnsubscribeKey(AEVK_4, InputManager::TRIGGERED, LoadUserMap1);
+	InputManager::UnsubscribeKey(AEVK_5, InputManager::TRIGGERED, LoadUserMap2);
+	InputManager::UnsubscribeKey(AEVK_6, InputManager::TRIGGERED, LoadUserMap3);
 }
 
 void SceneLevelEditor::Unload() {
-	return;
+	BuildingManager::Clear();
+}
+
+///////////////////////////////////////////////////////////////////////////
+// Map loading
+
+void LoadSelectedMap() {
+	GridManager::Free();
+	GridManager::Initialize(selectedMapFilePath);
+}
+
+void LoadMap1() {
+	selectedMapFilePath = "Assets/JSON_Data/Maps/map0.momomaps";
+	LoadSelectedMap();
+}
+
+void LoadMap2() {
+	selectedMapFilePath = "Assets/JSON_Data/Maps/map1.momomaps";
+	LoadSelectedMap();
+}
+
+void LoadMap3() {
+	selectedMapFilePath = "Assets/JSON_Data/Maps/map2.momomaps";
+	LoadSelectedMap();
+}
+
+void LoadUserMap1() {
+	selectedMapFilePath = "Assets/JSON_Data/Maps/userMap0.momomaps";
+	LoadSelectedMap();
+}
+
+void LoadUserMap2() {
+	selectedMapFilePath = "Assets/JSON_Data/Maps/userMap1.momomaps";
+	LoadSelectedMap();
+}
+
+void LoadUserMap3() {
+	selectedMapFilePath = "Assets/JSON_Data/Maps/userMap2.momomaps";
+	LoadSelectedMap();
 }
